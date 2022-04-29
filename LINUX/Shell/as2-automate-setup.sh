2d58cca89b92c623c7fe22d5956f0e81435d9b8a
Justin Grego <97710986+JGrego-AWS@users.noreply.github.com>
2022-04-29 14:15:55 -0400

    Cleaned up readability of bash script

--- LINUX/Shell/as2-automate-setup.sh
+++ LINUX/Shell/as2-automate-setup.sh
@@ -39,29 +39,31 @@ sudo chown as2-automation:as2-automation /home/as2-automation/.ssh
 sudo chown as2-automation:as2-automation /home/as2-automation/.ssh/authorized_keys
 
 # Generate script to create AppStream 2.0 application optimization manifest files
-echo "#!/bin/bash" >> /tmp/generate_appstream_manifest.sh
-echo "# usage generate_appstream_manfiest.sh app_full_path app_exe_name" >> /tmp/generate_appstream_manifest.sh
-echo >> /tmp/generate_appstream_manifest.sh
-echo "echo \"Begin execution of AppStream 2.0 app optimization manifest generation script.\"" >> /tmp/generate_appstream_manifest.sh
-echo "# Run passed process and wait 20sec for it to launch" >> /tmp/generate_appstream_manifest.sh
-echo "(\$1 &)" >> /tmp/generate_appstream_manifest.sh
-echo "echo \"Waiting 20sec for \$1 to completely launch.\"" >> /tmp/generate_appstream_manifest.sh
-echo "sleep 20s" >> /tmp/generate_appstream_manifest.sh
-echo >> /tmp/generate_appstream_manifest.sh
-echo "# Find lowest pid in process tree for \$2" >> /tmp/generate_appstream_manifest.sh
-echo "lowpid=\$(ps -C \$2 -o pid= |sort |head -n 1)" >> /tmp/generate_appstream_manifest.sh
-echo "if [[ \$lowpid ]]; then" >> /tmp/generate_appstream_manifest.sh
-echo "  echo \"The pid for \$2 is \$lowpid\"" >> /tmp/generate_appstream_manifest.sh
-echo >> /tmp/generate_appstream_manifest.sh
-echo "  # Generate list of files used by the running process" >> /tmp/generate_appstream_manifest.sh
-echo "  sudo lsof -p \$(pstree -p \$lowpid | grep -o '([0-9]\+)' | grep -o '[0-9]\+' | tr '\012' ,)|grep REG | sed -n '1!p' | awk '{print \$9}'|awk 'NF' >> /tmp/as2_manifest_\$2.txt" >> /tmp/generate_appstream_manifest.sh
-echo >> /tmp/generate_appstream_manifest.sh
-echo "  echo \"Terminating pid \$lowpid for app \$1\"" >> /tmp/generate_appstream_manifest.sh
-echo "  kill \$lowpid" >> /tmp/generate_appstream_manifest.sh
-echo "else" >> /tmp/generate_appstream_manifest.sh
-echo "  echo \"Unable to find the pid for \$1, no manfiest will be generated.\"" >> /tmp/generate_appstream_manifest.sh
-echo "fi" >> /tmp/generate_appstream_manifest.sh
-echo "echo \"End execution of AppStream 2.0 manifest generation script.\"" >> /tmp/generate_appstream_manifest.sh
+cat << EOF > /tmp/generate_appstream_manifest.sh
+#!/bin/bash
+# usage generate_appstream_manfiest.sh app_full_path app_exe_name
+
+echo "Begin execution of AppStream 2.0 app optimization manifest generation script."
+# Run passed process and wait 20sec for it to launch
+(\$1 &)
+echo "Waiting 20sec for \$1 to completely launch."
+sleep 20s
+
+# Find lowest pid in process tree for \$2
+lowpid=\$(ps -C \$2 -o pid= |sort |head -n 1)
+if [[ \$lowpid ]]; then
+  echo "The pid for \$2 is \$lowpid, generating manifest file."
+
+  # Generate list of files used by the running process
+  sudo lsof -p \$(pstree -p \$lowpid | grep -o '([0-9]\+)' | grep -o '[0-9]\+' | tr '\012' ,)|grep REG | sed -n '1!p' | awk '{print \$9}'|awk 'NF' >> /tmp/as2_manifest_\$2.txt 
+
+  echo "Terminating pid \$lowpid for app \$1"
+  kill \$lowpid
+else
+  echo "Unable to find the pid for \$1, no manfiest will be generated."
+fi
+echo "End execution of AppStream 2.0 manifest generation script."
+EOF
 
 # Make script executable
 chmod +x /tmp/generate_appstream_manifest.sh
